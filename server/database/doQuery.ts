f193dc8dcee4563dd6fb4c1c0fec81912254c444
jawoon1120 <jawoon@DESKTOP-DL4G6U4.localdomain>
2020-07-31 22:17:15 +0900

    추천 알고리즘 완료

--- server/database/doQuery.ts
+++ server/database/doQuery.ts
@@ -25,10 +25,15 @@ function doQuery<QueryResult = any>(
           connection.query(query, queryArray, (error: { sqlMessage: string | undefined; }, result: QueryResult) => {
             if (error) {
                 //connection.release();
+                console.log('query',query);
+                console.log('query',queryArray);
+                
               reject(new createError[500](error.sqlMessage));
             } else {
               //connection.release();
               //console.log("***************[Close Database Connection]***************");
+              console.log('query',query);
+              console.log('query',queryArray);
               resolve({ error, result });
             }
           });
--- server/routes/cocktail/cocktail.ts
+++ server/routes/cocktail/cocktail.ts
@@ -10,21 +10,43 @@ router.use('/detail',cocktailDetailRouter);
 router.route('/')
     .get(
         (req,res)=>{
-            const sql1 = 'SELECT drinkKategorie,userID FROM refrigerator WEHRE userId=?';
+            const sql1 = `SELECT drinkKategorie,userId FROM refrigerator WHERE userId = ?`
             const params1 = [req.user];
+            // console.log(req);
+            // console.log(req.user);
             dbQuery(sql1,params1).
-            then((row)=>{
-                if(row.result[0]){
-                    console.log("success");
-                    console.log(typeof(row.result[0]))
-                    
-                }
-                else{
-                    // nothing
-                    console.log("nothing");
-                    res.send('nothing');
-                }
-            })
+                then((row)=>{
+                    if(row.result[0]){
+                        console.log("success");
+                        console.log(row.result);
+                        var tokens = new Array(row.result.length).fill('?').join(',');
+                        let userDrink:Array<string>=[];
+                        row.result.forEach(function(element:{ drinkKategorie:string, userId: string}){
+                            console.log("element : "+element.drinkKategorie);
+                            console.log(typeof(element.drinkKategorie));
+                            userDrink.push(element.drinkKategorie);
+                        });
+                        // for(let i=0; i < row.result.length ;i++){
+                        
+                        //     userDrink+= "'"+row.result[i].drinkKategorie+"'";
+                        //     if(i<row.result.length-1)
+                        //         userDrink+=" OR "
+                        //     console.log(userDrink);
+                        // }
+                        //console.log(typeof(row.result[0]))
+                        const sql2 = `SELECT cocktailName FROM recipe WHERE ingredient IN(${tokens})`;
+                        console.log("userDrink:"+userDrink)
+                        dbQuery(sql2,userDrink)
+                            .then((row)=>{
+                                console.log(row.result);
+                            })
+                    }
+                    else{
+                        // nothing
+                        console.log("nothing");
+                        res.send('nothing');
+                    }
+                })
     })   
 
 
